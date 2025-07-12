f4d652cdcb3604c7fe18ed98099a86564fa7d080
Siddharth77s <siddharthshinde9476@gmail.com>
2025-07-12 18:20:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (63.78%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        while (root) {
+            if (!root->left) {
+                ans.push_back(root->val);
+                root = root->right;
+            } else {
+                TreeNode* prev = root->left;
+                while (prev->right && prev->right != root) {
+                    prev = prev->right;
+                }
+                if (!prev->right) {
+                    prev->right = root;
+                    root = root->left;
+                } else {
+                    ans.push_back(root->val);
+                    prev->right = nullptr;
+                    root = root->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
